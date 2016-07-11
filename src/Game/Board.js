c7fa1e3f262058b4b1b68f4d2ab1cd97ab399d55
vrama628 <vrama628@gmail.com>
2016-07-10 22:31:38 -0500

    made board deterministic and added robber

--- src/Game/Board.js
+++ src/Game/Board.js
@@ -11,27 +11,27 @@ const tileIDs = [50,51,52,40,28,15,2,1,0,12,24,37,38,39,27,14,13,25,26],
         'Wool', 'Wool', 'Wool', 'Wool',
         'Desert'
       ],
-      tileNumbers = [5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11],
+      tileNumbers = [5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11, undefined],
       intersectionIDs = [0,1,2,6,7,8,9,12,13,14,15,18,19,20,21,22,24,25,26,27,
         28,30,31,32,33,34,35,36,37,38,39,40,41,43,44,45,46,47,49,50,51,52,53,
         56,57,58,59,62,63,64,65,69,70,71];
 
-// returns a new array with elem inserted into arr at index, without mutating arr.
-function insert(arr, index, elem) {
-  return [].concat(arr.slice(0, index), [elem], arr.slice(index));
-}
 
 module.exports = class Board {
-  constructor() {
-    const shuffledTileTypes = _.shuffle(tileTypes),
-          shiftedTileNumbers =
-            insert(tileNumbers, shuffledTileTypes.indexOf('Desert'), undefined),
-          tiles = _.zip(tileIDs, shuffledTileTypes, shiftedTileNumbers)
-            .map(tileArr => ({
-              id: tileArr[0],
-              type: tileArr[1],
-              number: tileArr[2]
-            }));
+  constructor(options) {
+    if (!_.isEqual(_.pluck(options.tiles, 'type').sort(), tileTypes.sort())) {
+      throw new TypeError("Impossible tile type arrangement.");
+    }
+    if (!_.isEqual(_.pluck(options.tiles, 'number').sort(), tileNumbers.sort())) {
+      throw new TypeError("Impossible tile number arrangement.");
+    }
+    if (!_.isEqual(_.pluck(options.tiles, 'id').sort(), tileIDs.sort())) {
+      throw new TypeError("Impossible tile ID arrangement.");
+    }
+    if (options.tiles.find(tile => tile.type === 'Desert').number !== undefined) {
+      throw new TypeError("Impossible desert numbering.");
+    }
+    const tiles = options.tiles.map(_.clone);
     this.getTiles = () => tiles.map(_.clone);
 
     const intersections = {};
@@ -43,7 +43,7 @@ module.exports = class Board {
         type: 'Settlement',
         playerID: playerID
       }
-    }
+    };
     this.placeCity = (intersectionID, playerID) => {
       if (!_.contains(intersectionIDs, intersectionID))
         throw new Error('first argument must be a valid intersectionID.');
@@ -51,7 +51,7 @@ module.exports = class Board {
         type: 'City',
         playerID: playerID
       }
-    }
+    };
 
     const edges = _.object(intersectionIDs,
       _.times(intersectionIDs.length, () => ({})));
@@ -62,7 +62,17 @@ module.exports = class Board {
         throw new Error('first two arguments must be valid intersectionIDs.');
       edges[intersectionID1][intersectionID2] = playerID;
       edges[intersectionID2][intersectionID1] = playerID;
-    }
+    };
+
+    let robber = options.tiles.find(tile => tile.type === 'Desert').id;
+    this.getRobber = () => robber;
+    this.placeRobber = id => robber = id;
+
+    this.toPlain = () => ({
+      tiles: this.getTiles(),
+      intersections: _.mapObject(intersections, _.clone),
+      edges: _.mapObject(edges, _.clone)
+    });
   }
 
   static getIntersectionIDs() {
--- test/BoardTests.js
+++ test/BoardTests.js
@@ -2,42 +2,72 @@
 
 const expect = require('chai').expect;
 
-const Board = require('../src/game/Board');
+const Board = require('../src/Game/Board');
 
 describe('Board', function() {
+  let tiles;
+  beforeEach(function() {
+    tiles = [
+      {id:50, type:'Brick', number:5},
+      {id:51, type:'Brick', number:2},
+      {id:52, type:'Brick', number:6},
+      {id:40, type:'Lumber', number:3},
+      {id:28, type:'Lumber', number:8},
+      {id:15, type:'Lumber', number:10},
+      {id:2, type:'Lumber', number:9},
+      {id:1, type:'Ore', number:12},
+      {id:0, type:'Ore', number:11},
+      {id:12, type:'Ore', number:4},
+      {id:24, type:'Wheat', number:8},
+      {id:37, type:'Wheat', number:10},
+      {id:38, type:'Wheat', number:9},
+      {id:39, type:'Wheat', number:4},
+      {id:27, type:'Wool', number:5},
+      {id:14, type:'Wool', number:6},
+      {id:13, type:'Wool', number:3},
+      {id:25, type:'Wool', number:11},
+      {id:26, type:'Desert'},
+    ]
+  });
+
   describe('constructor', function() {
-    [
-      [3, 'Brick'],
-      [4, 'Lumber'],
-      [3, 'Ore'],
-      [4, 'Wheat'],
-      [4, 'Wool'],
-      [1, 'Desert']
-    ].map(testCase => {
-      let amount = testCase[0], type = testCase[1];
-      it(`constructs a board with ${amount} ${type} tiles when called with no arguments.`, function() {
-        let board = new Board();
-        expect(board.getTiles().filter(tile => tile.type === type)).to.have.lengthOf(amount);
-      });
+    it("throws a TypeError if the first argument is not an object with an array 'tiles' field.", function() {
+      tiles = [];
+      expect(() => new Board({tiles:tiles})).to.throw(TypeError);
     });
 
-    [
-      [1, 2],
-      [2, 3],
-      [2, 4],
-      [2, 5],
-      [2, 6],
-      [2, 8],
-      [2, 9],
-      [2, 10],
-      [2, 11],
-      [1, 12]
-    ].map(testCase => {
-      let amount = testCase[0], number = testCase[1];
-      it(`constructs a board with ${amount} tiles numbered ${number} when called with no arguments.`, function() {
-        let board = new Board();
-        expect(board.getTiles().filter(tile => tile.number === number)).to.have.lengthOf(amount);
-      });
+    it("throws a TypeError if there are not 3 'Brick's, 4 'Lumber's, 3 'Ore's, 4 'Wheat's, 4 'Wool's, and 1 'Desert' " +
+        "among the 'type' fields of the elements of the first argument's 'tiles' array.", function() {
+      tiles[0].type = 'Lumber';
+      expect(() => new Board({tiles:tiles})).to.throw(TypeError);
+    });
+
+    it("throws a TypeError if there are not 1 2, 2 3s, 2 4s, 2 5s, 2 6s, 2 8s, 2 9s, 2 10s, 2 11s, and 1 12 among the 'numbers' " +
+        "fields of the elements of the first argument's 'tiles' array.", function() {
+      tiles[0].number = 2;
+      expect(() => new Board({tiles:tiles})).to.throw(TypeError);
+    });
+
+    it("throws a TypeError if the 'id' fields of the elements of the first argument's 'tiles' array don't contain one each of " +
+        "50, 51, 52, 40, 28, 15, 2, 1, 0, 12, 24, 37, 38, 39, 27, 14, 13, 25, and 26.", function() {
+      tiles[0].id = 49;
+      expect(() => new Board({tiles:tiles})).to.throw(TypeError);
+    });
+
+    it("throws a TypeError if the tile with type 'Desert' does not have an undefined 'number' field.", function() {
+      tiles[18].number = 11;
+      tiles[17].number = undefined;
+      expect(() => new Board({tiles:tiles})).to.throw(TypeError);
+    });
+
+    it("constructs a board whose #getTiles() returns an array deeply equivalent to the first argument passed to the constructor.", function() {
+      let board = new Board({tiles:tiles});
+      expect(board.getTiles()).to.deep.equal(tiles);
+    });
+
+    it("constructs a board with the robber placed on the Desert tile.", function() {
+      let board = new Board({tiles:tiles});
+      expect(board.getRobber()).to.equal(26);
     });
   });
 
@@ -47,7 +77,7 @@ describe('Board', function() {
 
   describe('#getEdge(intersection1ID, intersection2ID)', function() {
     it("returns undefined if there is no road between the two intersections specified.", function() {
-      let board = new Board();
+      let board = new Board({tiles:tiles});
       expect(board.getEdge(0, 7)).to.equal(undefined);
     });
 
@@ -56,7 +86,7 @@ describe('Board', function() {
 
   describe('#getIntersection(intersectionID)', function() {
     it("returns undefined if there is no settlement or city on the specified intersection.", function() {
-      let board = new Board();
+      let board = new Board({tiles:tiles});
       expect(board.getIntersection(25)).to.equal(undefined);
     });
 
@@ -65,9 +95,13 @@ describe('Board', function() {
     it("returns an object with field type = 'City' and field playerID = the playerID of the owner of the city if there is one on the specified intersection.", () => "tested indirectly.");
   });
 
+  describe('#getRobber()', function() {
+    it("returns the tileID of the tile the robber is on.", () => "tested indirectly.");
+  });
+
   describe('#placeRoad(intersection1ID, intersection2ID, playerID)', function() {
     it("places a road connecting intersection1 and intersection2, owned by player.", function() {
-      let board = new Board();
+      let board = new Board({tiles:tiles});
       board.placeRoad(51, 57, 12345);
       expect(board.getEdge(57, 51)).to.equal(12345);
     });
@@ -75,7 +109,7 @@ describe('Board', function() {
 
   describe('#placeSettlement(intersectionID, playerID)', function() {
     it("places a settlement on intersection, owned by player.", function() {
-      let board = new Board();
+      let board = new Board({tiles:tiles});
       board.placeSettlement(50, 'some id');
       expect(board.getIntersection(50)).to.deep.equal({
         type: 'Settlement',
@@ -86,12 +120,43 @@ describe('Board', function() {
 
   describe('#placeCity(intersectionID, playerID)', function() {
     it("places a city on intersection, owned by player.", function() {
-      let board = new Board();
+      let board = new Board({tiles:tiles});
       board.placeCity(35, 628);
       expect(board.getIntersection(35)).to.deep.equal({
         type: 'City',
         playerID: 628
       });
+    });
+  });
+
+  describe('#placeRobber(tileID)', function() {
+    it("places the robber on tile.", function() {
+      let board = new Board({tiles:tiles});
+      board.placeRobber(12);
+      expect(board.getRobber()).to.equal(12);
+    });
+  })
+
+  describe('#toPlain()', function() {
+    it("returns an object with a 'tiles' field containing a result of #getTiles()", function() {
+      let board = new Board({tiles:tiles});
+      expect(board.toPlain().tiles).to.deep.equal(board.getTiles());
+    });
+
+    it("returns an object with an 'intersections' field containing an object whose ith field is the result of #getIntersection(i)", function() {
+      let board = new Board({tiles:tiles});
+      board.placeCity(21, 'player1');
+      board.placeSettlement(35, 'player2');
+      expect(board.toPlain().intersections).to.deep.equal({
+        21: board.getIntersection(21),
+        35: board.getIntersection(35)
+      });
+    });
+
+    it("returns an object with an 'edges' field containing an object whose ith field's jth field is the result of #getEdge(i,j)", function() {
+      let board = new Board({tiles:tiles});
+      board.placeRoad(25, 32, 'Jingles');
+      expect(board.toPlain().edges[25][32]).to.equal('Jingles');
     });
   });
 
